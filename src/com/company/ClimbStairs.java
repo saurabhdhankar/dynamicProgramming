b4577c006c8931d9291af7862699b99d5520721e
Saurabh Kumar <saurabhdhankar1992@gmail.com>
2023-07-11 19:20:03 +0530

    Climb stairs min cost.

--- src/com/company/ClimbStairs.java
+++ src/com/company/ClimbStairs.java
@@ -32,4 +32,20 @@ public class ClimbStairs {
         return pathsToDestinationCount.get(current);
     }
 
+    /*
+    You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+    You can either start from the step with index 0, or the step with index 1.
+    Return the minimum cost to reach the top of the floor.
+     */
+    public int minCostToClimbStairs(int[] costs) {
+        int prev1 = 0, prev2 = 0;
+        int i = 2;
+        for(; i < costs.length; i++) {
+            int curr = Math.min(prev1+costs[i-1], prev2+costs[i-2]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return Math.min(prev1+costs[i-1], prev2+costs[i-2]);
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,8 @@ public class Main {
 //        System.out.println("Nth Fibonacci number is : "+fibonacci.findNthFibonacciNumber(8));
 
         ClimbStairs climbStairs = new ClimbStairs();
-        System.out.println("All ways to reach the Nth Stair : "+ climbStairs.findAllWaysToReachToNthStair(4));
+//        System.out.println("All ways to reach the Nth Stair : "+ climbStairs.findAllWaysToReachToNthStair(4));
+        int[] costs = {1,100,1,1,1,100,1,1,100,1};
+        System.out.println("MinCost to climb the stairs : "+ climbStairs.minCostToClimbStairs(costs));
     }
 }
