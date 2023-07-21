0fc313cdae8c05a771b5d4cbb224ecfb8762ae57
Saurabh Kumar <saurabhdhankar1992@gmail.com>
2023-07-21 14:36:56 +0530

    Grid Min and Max path sum from any point to any point.

--- src/com/company/GridPathSum.java
+++ src/com/company/GridPathSum.java
@@ -49,4 +49,67 @@ public class GridPathSum {
         }
         return grid[m-1][n-1];
     }
+
+
+    /*
+    You have been given an N*M matrix filled with integer numbers,
+    find the maximum sum that can be obtained from a path starting from any cell in the first row to any cell in the last row.
+     */
+
+    public  int getMaxPathSum(int[][] grid) {
+        int n = grid.length;
+        if(n < 1) return 0;
+
+        int m = grid[0].length;
+        for(int i = 1; i < n; i++) {
+            if(m > 1) {
+                grid[i][0] += Math.max(grid[i-1][0], grid[i-1][1]);
+                grid[i][m-1] += Math.max(grid[i-1][m-2], grid[i-1][m-1]);
+            } else {
+                grid[i][0] += grid[i-1][0];
+            }
+
+            for(int j = 1; j < m-1; j++) {
+                int prevMax = Math.max(grid[i - 1][j - 1], grid[i - 1][j]);
+                prevMax = Math.max(prevMax, grid[i - 1][j + 1]);
+                grid[i][j] += prevMax;
+            }
+        }
+        int maxPathSum = grid[n-1][0];
+        for(int j = 0; j < m; j++) {
+            maxPathSum = Math.max(maxPathSum, grid[n-1][j]);
+        }
+        return maxPathSum;
+    }
+
+    /*
+    You have been given an N*M matrix filled with integer numbers,
+    find the minimum sum that can be obtained from a path starting from any cell in the first row to any cell in the last row.
+     */
+
+    public  int getMinPathSum(int[][] grid) {
+        int n = grid.length;
+        if(n < 1) return 0;
+
+        int m = grid[0].length;
+        for(int i = 1; i < n; i++) {
+            if(m > 1) {
+                grid[i][0] += Math.min(grid[i-1][0], grid[i-1][1]);
+                grid[i][m-1] += Math.min(grid[i-1][m-2], grid[i-1][m-1]);
+            } else {
+                grid[i][0] += grid[i-1][0];
+            }
+
+            for(int j = 1; j < m-1; j++) {
+                int prevMax = Math.min(grid[i - 1][j - 1], grid[i - 1][j]);
+                prevMax = Math.min(prevMax, grid[i - 1][j + 1]);
+                grid[i][j] += prevMax;
+            }
+        }
+        int minPathSum = grid[n-1][0];
+        for(int j = 0; j < m; j++) {
+            minPathSum = Math.min(minPathSum, grid[n-1][j]);
+        }
+        return minPathSum;
+    }
 }
